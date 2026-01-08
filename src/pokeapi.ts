1707f58ff863efc00d1271937571d368dbdf7f39
Carrou122 <sufiadzhar122@gmail.com>
2026-01-08 17:42:56 +0800

    pokecache

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,11 +1,16 @@
+import { Cache } from "./pokecache.js";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
-
-  constructor() {}
+  private cache: Cache; 
+  constructor() {
+    this.cache = new Cache(3000);
+  }
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
     const url = pageURL || `${PokeAPI.baseURL}/location-area`;
-
+    const cached = this.cache.get<ShallowLocations>(url);
+    if (cached) return cached;
     try {
       const resp = await fetch(url);
 
@@ -14,6 +19,7 @@ export class PokeAPI {
       }
 
       const locations: ShallowLocations = await resp.json();
+      this.cache.add(url, location);
       return locations;
     } catch (e) {
       throw new Error(`Error fetching locations: ${(e as Error).message}`);
@@ -22,7 +28,8 @@ export class PokeAPI {
 
   async fetchLocation(locationName: string): Promise<Location> {
     const url = `${PokeAPI.baseURL}/location-area/${locationName}`;
-
+    const cached = this.cache.get<Locations>(url);
+    if (cached) return cached;
     try {
       const resp = await fetch(url);
 
@@ -31,6 +38,7 @@ export class PokeAPI {
       }
 
       const location: Location = await resp.json();
+      this.cache.add(url, location);
       return location;
     } catch (e) {
       throw new Error(
